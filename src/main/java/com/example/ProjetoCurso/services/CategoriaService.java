bc6b593e4062f0392c06c45c6aa5d1fc29fa530f
Herlander <herlander97dasilva@gmail.com>
2021-02-08 19:43:48 -0300

    Ajustes no end-point/categorias/id

--- src/main/java/com/example/ProjetoCurso/services/CategoriaService.java
+++ src/main/java/com/example/ProjetoCurso/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.example.ProjetoCurso.services;
 
 import com.example.ProjetoCurso.domain.Categoria;
 import com.example.ProjetoCurso.repositories.CategoriaRepository;
+import com.example.ProjetoCurso.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,8 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id){
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
 
     }
