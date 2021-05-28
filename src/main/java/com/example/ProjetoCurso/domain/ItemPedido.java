f1293d35c5be700cce449a0fb735c7cd47fb3a7e
Herlander <herlander97dasilva@gmail.com>
2021-05-27 22:11:30 -0300

    Implementando toString de Pedido

--- src/main/java/com/example/ProjetoCurso/domain/ItemPedido.java
+++ src/main/java/com/example/ProjetoCurso/domain/ItemPedido.java
@@ -1,10 +1,13 @@
 package com.example.ProjetoCurso.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.apache.tomcat.jni.Local;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Objects;
 
 @Entity
@@ -95,5 +98,21 @@ public class ItemPedido implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+        final StringBuffer sb = new StringBuffer("ItemPedido{");
+        sb.append(getProduto().getNome());
+        sb.append(", Qte: ");
+        sb.append(getQuantidade());
+        sb.append(", Preço unitário: ");
+        sb.append(nf.format(getPreço()));
+        sb.append(", Subtotal: ");
+        sb.append(nf.format(getSubTotal()));
+        sb.append("\n");
+
+        return sb.toString();
     }
 }
