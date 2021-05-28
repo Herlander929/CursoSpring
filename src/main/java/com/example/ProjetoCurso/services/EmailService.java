3fd3d3e39c03eed42243afc08b6ab181bc3ffd86
Herlander <herlander97dasilva@gmail.com>
2021-05-28 16:56:49 -0300

    Configuração inicial do Spring security

--- src/main/java/com/example/ProjetoCurso/services/EmailService.java
+++ src/main/java/com/example/ProjetoCurso/services/EmailService.java
@@ -3,9 +3,16 @@ package com.example.ProjetoCurso.services;
 import com.example.ProjetoCurso.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
+
 }
