45e6e55f4e9d3bf28a39df9a304581ea131112bb
Herlander <herlander97dasilva@gmail.com>
2021-05-31 11:21:57 -0300

    Adicionando senha a cliente

--- src/main/java/com/example/ProjetoCurso/domain/Cliente.java
+++ src/main/java/com/example/ProjetoCurso/domain/Cliente.java
@@ -24,6 +24,10 @@ public class Cliente implements Serializable {
     private Integer tipo;
 
 
+    @JsonIgnore
+    private String senha;
+
+
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
     private List<Endereco> enderecos = new ArrayList<>();
 
@@ -39,12 +43,13 @@ public class Cliente implements Serializable {
 
     }
 
-    public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+    public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
         this.id = id;
         this.nome = nome;
         this.email = email;
         this.cpfOuCnpj = cpfOuCnpj;
         this.tipo = (tipo == null) ? null : tipo.getCod();
+        this.senha = senha;
     }
 
     public Integer getId() {
@@ -87,6 +92,14 @@ public class Cliente implements Serializable {
         this.tipo = tipo.getCod();
     }
 
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
     public List<Pedido> getPedidos() {
         return pedidos;
     }
@@ -123,4 +136,6 @@ public class Cliente implements Serializable {
     public void setEnderecos(List<Endereco> enderecos) {
         this.enderecos = enderecos;
     }
+
+
 }
