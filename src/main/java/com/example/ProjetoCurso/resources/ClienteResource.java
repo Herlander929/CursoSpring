bf8507a0b397d6871f9fd32ecb06c828f5c816d9
Herlander <herlander97dasilva@gmail.com>
2021-06-01 19:07:59 -0300

    Autoriazando endpoints para perfis especificos

--- src/main/java/com/example/ProjetoCurso/resources/ClienteResource.java
+++ src/main/java/com/example/ProjetoCurso/resources/ClienteResource.java
@@ -10,6 +10,7 @@ import com.example.ProjetoCurso.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -48,18 +49,21 @@ public class ClienteResource {
         return ResponseEntity.noContent().build();
 
     }
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> delete (@PathVariable Integer id){
         service.delete(id);
         return ResponseEntity.noContent().build();
 
     }
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping( method = RequestMethod.GET)
     public ResponseEntity<List<ClienteDTO>> findAll(){
         List<Cliente> list = service.findAll();
         List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
     }
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping( value = "/page", method = RequestMethod.GET)
     public ResponseEntity<Page<ClienteDTO>> findPage(
             @RequestParam(value = "page", defaultValue = "0") Integer page,
