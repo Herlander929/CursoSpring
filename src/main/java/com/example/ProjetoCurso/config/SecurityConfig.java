308cf6e8e97f0e7a884fce100e677a8455d977a2
Herlander <herlander97dasilva@gmail.com>
2021-06-01 00:16:30 -0300

    Implementando autorizacao

--- src/main/java/com/example/ProjetoCurso/config/SecurityConfig.java
+++ src/main/java/com/example/ProjetoCurso/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.ProjetoCurso.config;
 
 import com.example.ProjetoCurso.security.JWTAuthenticationFilter;
+import com.example.ProjetoCurso.security.JWTAuthorizationFilter;
 import com.example.ProjetoCurso.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -53,6 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
